40371a773028fa541e667222610745f936e6391d
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-11-01 14:32:32 +0100

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+
+function Navbar() {
+  // Subscribe to the AuthContext to gain access to
+  // the values from AuthContext.Provider `value` prop
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  // Subscribe to the AuthContext to gain access to
+  // the values from AuthContext.Provider `value` prop
+  return (
+    <nav>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+
+      {isLoggedIn && (
+        <>
+          <Link to="/projects">
+            <button>Projects</button>
+          </Link>
+
+          <button onClick={logOutUser}></button>
+          <span>{user && user.name}</span>
+
+          <button>Logout</button>
+        </>
+      )}
+
+      {!isLoggedIn && (
+        <>
+          <Link to="/signup">
+            {" "}
+            <button>Sign Up</button>{" "}
+          </Link>
+          <Link to="/login">
+            {" "}
+            <button>Login</button>{" "}
+          </Link>
+        </>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
